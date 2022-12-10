82e6271148c1e7ad9f61bbe4a59d317da81548c2
Никита Хандрико <nikh.17@mail.ru>
2022-12-10 21:27:25 +0300

    Add project files.

---
+++ KPI/KPI/App.xaml.cs
@@ -0,0 +1,38 @@
+﻿using KPI.Services;
+using KPI.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace KPI
+{
+    public partial class App : Application
+    {
+        
+        public static string passwordMemory { set; get; }
+        public static string loginMemory { set; get; }
+
+        public App()
+        {
+            InitializeComponent();
+
+            passwordMemory = string.Empty;
+            loginMemory = string.Empty;
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+
+            Shell.Current.GoToAsync("//LoginPage");
+        }
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ KPI/KPI/Views/LoginPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using KPI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Syncfusion.XForms;
+
+namespace KPI.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+            this.BindingContext = new LoginViewModel();
+
+            login.Text = App.passwordMemory;
+            password.Text = App.passwordMemory;
+
+            loginSettings.Hint = "Логин";
+            loginSettings.EnableHintAnimation = true;
+
+            passwordSettings.Hint = "Пароль";
+            passwordSettings.EnableHintAnimation = true;
+            passwordSettings.EnablePasswordVisibilityToggle = true;
+        }
+
+        private void Login_Clicked(object sender, EventArgs e)
+        {
+            App.Current.MainPage = new AppShell();
+            App.passwordMemory = password.Text;
+            App.loginMemory = login.Text;
+        }
+    }
+}
